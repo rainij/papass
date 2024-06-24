7ede0b86a113e23490540bffa75d5add72813511
Reinhard Stahn <reinhardstahn@t-online.de>
2024-06-24 21:26:37 +0200

    Added a few cmd options

--- src/main.py
+++ src/main.py
@@ -2,9 +2,15 @@ import click
 
 
 @click.command()
-@click.option("--count", "-c", type=int, help="Number of words to generate.")
-def main(count):
-    print(f"Got: count={count}")
+@click.option(
+    "--count", "-c", type=int, required=True, help="Number of words to generate."
+)
+@click.option(
+    "--random-source", "-r", default="system", help="Source of randomness (system, dice)."
+)
+@click.option("--delimiter", "-d", default=" ", help="Separator between the words.")
+def main(count, random_source, delimiter):
+    print(f"Got: count={count}, random_source={random_source}, delimiter='{delimiter}'.")
 
 
 if __name__ == "__main__":
