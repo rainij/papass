653d086dfa14974174d22b4f58cef432cb17f1e5
Reinhard Stahn <reinhardstahn@t-online.de>
2024-06-26 20:25:40 +0200

    Options for WordList

--- src/main.py
+++ src/main.py
@@ -2,7 +2,7 @@ from pathlib import Path
 
 import click
 from phrase_generator import RandomPhraseGenerator
-from random_number_generator.dice import DiceRng
+from random_number_generator.system import SystemRng
 from wordlist import WordList
 
 
@@ -14,16 +14,32 @@ from wordlist import WordList
     "--random-source", "-r", default="system", help="Source of randomness (system, dice)."
 )
 @click.option("--delimiter", "-d", default=" ", help="Separator between the words.")
+@click.option(
+    "--min-word-size",
+    "--minw",
+    type=int,
+    default=1,
+    help="Filter out words which are shorter than this.",
+)
+@click.option(
+    "--max-word-size",
+    "--maxw",
+    type=int,
+    help="Filter out words which are longer than this.",
+)
 @click.argument("wordlist")
-def main(count, random_source, delimiter, wordlist):
-    print(
-        f"Got: count={count}, random_source={random_source}, delimiter='{delimiter}', wordlist={wordlist}."
+def main(count, random_source, delimiter, min_word_size, max_word_size, wordlist):
+    """Create a passphrase"""
+
+    rng = SystemRng(None)
+
+    wordlist = WordList.from_file(
+        Path(__file__).parent.parent.parent / "eff_large.wordlist",  # TODO
+        min_word_size=min_word_size,
+        max_word_size=max_word_size,
     )
 
-    rng = DiceRng(None)
-    wordlist = WordList.from_file(
-        Path(__file__).parent.parent.parent / "eff_large.wordlist", None
-    )  # TODO
+    print(f"Wordlist size: {len(wordlist)}")
 
     gen = RandomPhraseGenerator(wordlist, rng)
     phrase = gen.get_phrase(count)
--- src/wordlist.py
+++ src/wordlist.py
@@ -8,10 +8,28 @@ class WordList(Sequence[str]):
 
     _words: list[str]
 
-    def __init__(self, words: Iterable[str], config):
-        # TODO: validation, transformation
+    def __init__(
+        self,
+        words: Iterable[str],
+        *,
+        min_word_size: int = 1,
+        max_word_size: int | None = None,
+    ):
+        """Construct a wordlist from words.
+
+        :param words: Words to construct wordlist from.
+        :param min_word_size: Filter out words which are shorter than this.
+        :param max_word_size: Filter out words which are longer than this. None means no filtering.
+        """
+        words = list(words)
         self._validate(words)
-        self._words = list(words)
+        self._words = words
+
+        assert min_word_size > 0
+        assert max_word_size is None or max_word_size >= min_word_size
+
+        self._filter_min_word_size(min_word_size)
+        self._filter_max_word_size(max_word_size)
 
     def __getitem__(self, index):
         return self._words[index]
@@ -20,13 +38,17 @@ class WordList(Sequence[str]):
         return len(self._words)
 
     @staticmethod
-    def from_file(path: Path, config):
-        with open(path, "r") as f:
+    def from_file(file_path: Path, **options):
+        """Construct a wordlist from a file of words (newline separated).
+
+        The options are the same as the kwargs for the __init__ function.
+        """
+        with open(file_path, "r") as f:
             words = [w.strip() for w in f.readlines()]
-            return WordList(words, config)
+            return WordList(words, **options)
 
     @staticmethod
-    def _validate(words):
+    def _validate(words: list[str]):
         # TODOs:
         # - testing
         # - maybe use something else then assert. Better messages (logging)
@@ -38,3 +60,12 @@ class WordList(Sequence[str]):
             allowed_pattern.match(w) for w in words
         ), f"Words should match pattern '{allowed_pattern}'."
         assert len(set(words)) == len(words), "All words must be different."
+
+    def _filter_min_word_size(self, min_word_size: int) -> None:
+        self._words = [w for w in self._words if len(w) >= min_word_size]
+
+    def _filter_max_word_size(self, max_word_size: int | None) -> None:
+        if max_word_size is None:
+            return
+
+        self._words = [w for w in self._words if len(w) <= max_word_size]
