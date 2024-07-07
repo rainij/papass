01e5c8578d8b2a9e15f9ecc7bb8ae202bc9819b1
Reinhard Stahn <reinhardstahn@t-online.de>
2024-07-07 16:29:05 +0200

    Test registry

---
+++ tests/rspass/random/test_registry.py
@@ -0,0 +1,33 @@
+import pytest
+import rspass.random.registry
+from rspass.random.registry import (
+    available_random_sources,
+    default_random_source,
+    get_rng,
+)
+
+from tests.utils.cycle_rng import CycleRng
+
+
+def test_default_random_source():
+    assert default_random_source()
+
+
+def test_available_random_sources():
+    assert len(available_random_sources()) >= 2
+
+
+@pytest.mark.parametrize(
+    "random_source,options", [("cycle_rng", dict(cycle_from_cmd=[0, 1]))]
+)
+def test_get_rng(monkeypatch, random_source, options):
+    monkeypatch.setattr(
+        rspass.random.registry,
+        "_rng_registry",
+        dict(cycle_rng=(CycleRng, {"cycle": "cycle_from_cmd"})),
+    )
+
+    rng = get_rng(random_source, **options)
+
+    assert isinstance(rng, CycleRng)
+    assert [0, 1] == [rng.randbelow(2) for _ in range(2)]
