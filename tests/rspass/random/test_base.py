e37c72a1e7da9414b32a0dae20f5f8b9016520b2
Reinhard Stahn <reinhardstahn@t-online.de>
2024-07-07 11:42:12 +0200

    Improve test_base

--- tests/rspass/random/test_base.py
+++ tests/rspass/random/test_base.py
@@ -4,16 +4,18 @@ from rspass.random.base import RandomNumberGeneratorBase
 from tests.utils.cycle_rng import CycleRng
 
 
-class TestRandomNumberGeneratorBase:
-    @pytest.fixture
-    def cycle(self) -> list[int]:
-        return [0, 3, 2, 1]
+def get_cycle():
+    return [0, 3, 2, 1]
 
+
+class TestRandomNumberGeneratorBase:
     @pytest.fixture(scope="class")
     def cycle_rng(self) -> RandomNumberGeneratorBase:
-        return CycleRng([0, 3, 2, 1])
+        return CycleRng(get_cycle())
 
     @pytest.mark.parametrize("i", range(4))
-    def test_choice_uses_randbelow_as_desired(self, i, cycle, cycle_rng):
+    def test_choice_uses_randbelow_as_desired(self, i, cycle_rng):
         items = [10, 11, 12, 13]
+        cycle = get_cycle()
+
         assert cycle_rng.choice(items) == items[cycle[i]]
