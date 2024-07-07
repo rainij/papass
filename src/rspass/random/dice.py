97868808a220d434edacc513483412a17b87a036
Reinhard Stahn <reinhardstahn@t-online.de>
2024-07-07 14:34:07 +0200

    Refactor DiceRng

--- src/rspass/random/dice.py
+++ src/rspass/random/dice.py
@@ -19,26 +19,21 @@ class DiceRng(RandomNumberGeneratorBase):
         # - better error handling and user feedback
         num_sides = self._num_sides
 
-        num_rolls = 1
+        required_num_rolls = 1
         upper_dice = num_sides
 
         while upper_dice < upper:
-            num_rolls += 1
+            required_num_rolls += 1
             upper_dice *= num_sides
 
         upper_multiple = (upper_dice // upper) * upper
 
         result = None
         while result is None:
-            user_input = input(f"Roll {num_rolls} dice: ")
-            rolls = [int(r) for r in user_input.split()]
+            user_input = input(f"Roll at least {required_num_rolls} dice: ")
+            rolls = self._parse_user_input(user_input, required_num_rolls)
 
-            if len(rolls) < num_rolls:
-                print(f"Got only {len(rolls)} rolls, need {num_rolls}. Roll again!")
-                continue
-
-            if not all(1 <= r <= num_sides for r in rolls):
-                print(f"Some rolls are not between 1 and {num_sides}. Roll again!")
+            if rolls is None:
                 continue
 
             result = reduce(lambda acc, r: num_sides * acc + r - 1, rolls, 0)
@@ -51,3 +46,25 @@ class DiceRng(RandomNumberGeneratorBase):
             result = result % upper
 
         return result
+
+    def _parse_user_input(
+        self, user_input: str, required_num_rolls: int
+    ) -> list[int] | None:
+        """Returns None if user input could not be parsed."""
+        try:
+            rolls = [int(r) for r in user_input.split()]
+        except ValueError:
+            print(
+                "Invalid. Require a space-separated list of integers (like: 1 3 2). Roll again!"
+            )
+            return None
+
+        if len(rolls) < required_num_rolls:
+            print(f"Got only {len(rolls)} rolls, need {required_num_rolls}. Roll again!")
+            return None
+
+        if not all(1 <= r <= self._num_sides for r in rolls):
+            print(f"Some rolls are not between 1 and {self._num_sides}. Roll again!")
+            return None
+
+        return rolls
