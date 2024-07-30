2d0c1eb54bb62241fc091983e8775590c115ecba
Reinhard Stahn <reinhardstahn@t-online.de>
2024-07-30 17:21:13 +0200

    Make QueryStdinForDice public to papass

--- src/papass/__init__.py
+++ src/papass/__init__.py
@@ -75,6 +75,7 @@ the ``alphabet`` like e.g ``'3002212230'``. The ``entropy`` is ``20.0`` because 
 from .passphrase_generator import PassphraseGenerator, PassphraseResult
 from .password_generator import PasswordGenerator, PasswordResult
 from .random import DiceRng, QueryForDice, RngBase, SystemRng
+from .utils import QueryStdinForDice
 from .wordlist import WordList
 
 __version__ = "0.0.4"
@@ -86,6 +87,7 @@ __all__ = [
     PasswordGenerator.__name__,
     PasswordResult.__name__,
     QueryForDice.__name__,
+    QueryStdinForDice.__name__,
     RngBase.__name__,
     SystemRng.__name__,
     WordList.__name__,
